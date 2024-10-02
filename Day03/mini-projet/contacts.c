0a9bc9fd2edde51cf324d0a3059b7674ffe3feaf
Domain.Expansion.404 <chainsawman.11294@hotmail.com>
2024-10-02 12:47:45 +0200

    .

--- Day03/mini-projet/contacts.c
+++ Day03/mini-projet/contacts.c
@@ -1,7 +1,7 @@
-#include stdio.h
-#include string.h
-#include ctype.h
-#include contacts.h
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "contacts.h"
 
 void Add(contact user[], int num) {
     for (int i = 0; i < num; i++) {
